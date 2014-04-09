168082be26e6dd3387c04f8d06b4f0267b6d679f
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2014-04-10 00:57:43 +0900

    Fix for composer

--- src/ImageInspector/Application.php
+++ src/ImageInspector/Application.php
@@ -1,25 +1,50 @@
 <?php
 
-$files = array_slice($argv, 1);
+namespace ImageInspector;
 
-$images_info = array();
+class Application
+{
 
-foreach ($files as $file) {
+    public static function render($argv)
+    {
+        $images_info = array();
 
-    $image = new Imagick();
-    $image->readImage($file);
+        foreach ($argv as $file) {
 
-    $image->setFirstIterator();
+            $image = new \Imagick();
+            $image->readImage($file);
+
+            $image->setFirstIterator();
 
 
-    $images_info[] = array(
-        'filename' => basename($file),
-        'width' => $image->getImageWidth(),
-        'height' => $image->getImageHeight(),
-        'size' => $image->getImageLength(),
-        'format' => $image->getImageFormat(),
-    );
+            $images_info[] = array(
+                'filename' => basename($file),
+                'width' => $image->getImageWidth(),
+                'height' => $image->getImageHeight(),
+                'size' => $image->getImageLength(),
+                'format' => $image->getImageFormat(),
+            );
+
+        }
+
+        include __DIR__ . '/index.php';
+    }
+
+    public static function openBrowser($html, array $images)
+    {
+        $tmpdir = tempnam(sys_get_temp_dir(), 'imageinspector_');
+        $tmpdir_image = $tmpdir . '/images';
+
+        unlink($tmpdir);
+        mkdir($tmpdir);
+        mkdir($tmpdir_image);
+
+        file_put_contents($tmpdir . '/index.html', $html);
+        foreach ($images as $image) {
+            copy($image, $tmpdir_image . '/' . basename($image));
+        }
+        exec("open $tmpdir/index.html");
+    }
 
 }
 
-include __DIR__ . '/index.php';
